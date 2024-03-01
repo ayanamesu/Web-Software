7bed164d01ce4a596990aef6191454fb8786caa5
ayanamesu <leejacky74@gmail.com>
2024-02-29 20:02:45 -0800

    fixing web-software from scratch

--- routes/post.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../conf/database');
-const sharp = require('sharp');
-const multer = require('multer');
-
-
-module.exports = router;
